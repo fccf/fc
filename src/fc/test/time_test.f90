b381e147265f0e1fe53b7077c393eacf6d23ae14
fccf <xiyanfc@qq.com>
2018-09-22 23:01:11 +0800

    test timer

--- src/fc/test/time_test.f90
+++ src/fc/test/time_test.f90
@@ -1,7 +1,56 @@
 program time_test
   use time
+  use string
+  use iso_c_binding
   implicit none
 
+  real :: t1
+
+  print*, '# time_date '
   print*, now()
+  print*,s2hms(36009.7)
+
+  t1 = wtime()
+
+  call sleep(1)
+  print*, '# wtime '
+  print*, to_str(wtime())
+
+  call timer_test
+
+contains
+
+  subroutine timer_test
+    use time
+    implicit none
+
+    type(timer) :: tt
+
+    call tt%start('A')
+    call sleep(1)
+    call tt%stop('A')
+
+    call tt%start('A')
+    call sleep(1)
+    call tt%stop('A')
+
+    call tt%start('B')
+    call sleep(1)
+    call tt%stop('B')
+
+    call tt%start('C')
+    call sleep(1)
+    call tt%start('D')
+    call sleep(1)
+    call tt%stop('D')
+    call tt%stop('C')
+
+    print*, '# csv timer report'
+    call tt%report(unit = 6, fmt = 'csv')
+
+    print*, '# table timer report'
+    call tt%report(unit = 6, fmt = 'tbl')
+
+  end subroutine timer_test
 
 end program time_test
