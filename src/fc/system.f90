2ea925d119c878d4251260687bb889937dc5cd0c
fccf <xiyanfc@qq.com>
2018-09-22 21:03:48 +0800

    system cmd

---
+++ src/fc/system.f90
@@ -0,0 +1,40 @@
+module system
+
+  implicit none
+
+  public :: mkdir, cwd, cd
+
+  private
+
+contains
+  !=============================================================================
+  subroutine mkdir(dir)
+    !< make directory
+    character(*), intent(in):: dir
+
+    call system('mkdir '//dir)
+
+  end subroutine mkdir
+  !=============================================================================
+  function cwd() result(path)
+    !< get current work directory
+    character(:),allocatable :: path
+    character(256) :: path_
+
+    integer :: ierr
+
+    ierr = getcwd(path_)
+    path = trim(adjustl(path_))
+
+  end function cwd
+  !=============================================================================
+  subroutine cd(dir)
+    !< change directory
+    character(*),intent(in) :: dir
+    integer :: ierr
+
+    ierr = chdir(dir)
+
+  end subroutine cd
+
+end module system
